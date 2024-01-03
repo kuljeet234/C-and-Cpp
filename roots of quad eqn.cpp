f6598924ca5d15ff3c7389233fdea25fb350c474
kuljeet singh shekhawat <kuljeet.229310234@muj.manipal.edu>
2024-01-04 00:53:21 +0530

    Add files via upload

---
+++ roots of quad eqn.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+#include<math.h>
+using namespace std;
+int main()
+{
+float a,b,c,r1,r2;
+cout<<"enter a,b,c";
+cin>>a>>b>>c;
+r1=(-b+sqrt(b*b-4*a*c))/(2*a);
+r2=(-b-sqrt(b*b-4*a*c))/(2*a);
+cout<<"roots are"<<r1<<" "<<r2;
+return 0;
+}
